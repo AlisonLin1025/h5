737174f492097c8a84fb41e488189988426bd55a
alison_lin <1343815834@qq.com>
2016-04-17 23:26:56 +0800

    提交

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var msgList = getEle('.msgList');
 var msgLis = document.querySelectorAll('.message li');
 var cube = getEle('.cube');
 var cubeBox = getEle('.cubeBox');
-var cubeLis = cubeBox.querySelectorAll('li');
+var cubeLis = cubeBox.querySelectorAll('.cubeBox>li');
 var winW = window.innerWidth;
 var winH = window.innerHeight;
 var desW = 640;
@@ -102,8 +102,38 @@ var fnPhone = {
 
 }
 
-
 function fnCube(){
+    var startTouch = {x:0,y:0};
+    var startX = -45;
+    var startY = -45;
+    cubeBox.style.webkitTransform = "scale(0.7) rotateX(-45deg) rotateY(-45deg)";
+   [].forEach.call(cubeLis,function(){
+       arguments[0].addEventListener('touchstart',start,false)
+       arguments[0].addEventListener('touchmove',move,false)
+       arguments[0].addEventListener('touchend',end,false);
+   })
+    function start(e){
+        startTouch.x = e.changedTouches[0].pageX;
+        startTouch.y = e.changedTouches[0].pageY;
+    }
+    function move(e){
+        var moveTouchX = e.changedTouches[0].pageX;
+        var moveTouchY = e.changedTouches[0].pageY;
+        this.changePosX = moveTouchX - startTouch.x;
+        this.changePosY = moveTouchY - startTouch.y;
+        this.parentNode.style.webkitTransform = "scale(0.7)  rotateX("+(-startY-this.changePosY)+"deg) rotateY("+(startX+this.changePosX)+"deg)";
+    }
+    function end(){
+        startX+=this.changePosX;
+        startY+=this.changePosY;
+    }
+}
+document.addEventListener('touchstart',function(){
+},false);
+
+
+
+/*function fnCube(){
         var TouchStart = {x:0,y:0};
         var startX = -45;
         var startY = -45;
@@ -150,7 +180,7 @@ function fnCube(){
          document.removeEventListener('touchemove',cMove,false);
          document.removeEventListener('touchend',cEnd,false)
     }
-}
+}*/
 
 
 
